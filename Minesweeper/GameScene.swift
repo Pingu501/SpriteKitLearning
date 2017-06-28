55d71d34ecc98d38e1c6793722f983cc5741ad99
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2017-06-28 14:26:48 +0200

    INITIAL COMMIT

--- Minesweeper/GameScene.swift
+++ Minesweeper/GameScene.swift
@@ -8,82 +8,173 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var motionManager = CMMotionManager()
+    
+    private var deleteButton : SKNode! = nil
+    
+    private var gravity = false
+    private var gravityButton : SKNode! = nil
+    
+    private var currentlyDraggedBall : SKShapeNode?
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        backgroundColor = SKColor.darkGray
+        
+        self.motionManager.startAccelerometerUpdates()
+        
+        physicsWorld.gravity = CGVector.zero
+        physicsWorld.contactDelegate = self
+        
+        let border = SKNode.init()
+        border.position = CGPoint(x: 0, y: 0)
+        border.physicsBody = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.addChild(border)
+        
+        addDeleteButton()
+        addGravityButton()
+    }
+    
+    func addDeleteButton() {
+        self.deleteButton = SKSpriteNode(color: SKColor.black.withAlphaComponent(0.5), size: CGSize(width: 44, height: 44))
+        self.deleteButton.position = CGPoint(x: 44, y: 44)
+        self.deleteButton.zPosition = 1000;
+        self.deleteButton.name = "delete"
+        
+        let deleteText = SKLabelNode(text: "x") as SKNode
+        deleteText.position = CGPoint(x: 0, y: -10)
+        deleteText.name = "delete"
+        
+        self.deleteButton.addChild(deleteText)
+        self.addChild(self.deleteButton)
+    }
+    
+    func addGravityButton() {
+        self.gravityButton = SKSpriteNode(color: SKColor.black.withAlphaComponent(0.5), size: CGSize(width: 44, height: 44))
+        self.gravityButton.position = CGPoint(x: 100, y: 44)
+        self.gravityButton.zPosition = 1000
+        self.gravityButton.name = "gravitiy"
+        
+        let gravityText = SKLabelNode(text: "G") as SKNode
+        gravityText.position = CGPoint(x: 0, y: -10)
+        gravityText.name = "gravity"
+        gravityText.alpha = 0.1
+        
+        self.gravityButton.addChild(gravityText)
+        self.addChild(self.gravityButton)
+    }
+    
+    func handleGravityChange() {
+        let buttonText = self.gravityButton.childNode(withName: "gravity")
+        
+        if self.gravity {
+            self.physicsWorld.gravity = CGVector.zero
+            buttonText?.alpha = 0.1
+        } else {
+            buttonText?.alpha = 1
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        self.gravity = !self.gravity
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        guard let touch = touches.first else {
+            return
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        let touchedNodes = nodes(at: touch.location(in: self))
+        if (touchedNodes.first == nil) {
+            self.addBall(touch: touch)
+            
+        } else {
+            currentlyDraggedBall = touchedNodes.first as? SKShapeNode
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        guard let touch = touches.first else {
+            return
+        }
+        
+        if (currentlyDraggedBall == nil) {
+            let touchedNodes = nodes(at: touch.location(in: self))
+            currentlyDraggedBall = touchedNodes.first as? SKShapeNode
+        } else {
+            currentlyDraggedBall?.position = touch.location(in: self)
+            self.shotBall(ball: self.currentlyDraggedBall!, touch: touch)
+        }
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let touchedNodes = self.nodes(at: touch.location(in: self))
+        if let name = touchedNodes.first?.name {
+            switch name {
+            case "delete":
+                for node:SKNode in self.children {
+                    if node.name == "ball" {
+                        node.removeFromParent()
+                    }
+                }
+                break
+            case "gravity":
+                handleGravityChange()
+            default:
+                if currentlyDraggedBall != nil {
+                    self.shotBall(ball: currentlyDraggedBall!, touch: touch)
+                }
+            }
+        }
+            
+        currentlyDraggedBall = nil
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func addBall(touch : UITouch) {
+        let circleSize = CGFloat(20)
+        let colors = [SKColor.white, SKColor.black]
+        let randomIndex = Int(arc4random_uniform(UInt32(colors.count)))
+        let color = colors[randomIndex]
+        
+        let ball = SKShapeNode(circleOfRadius: circleSize)
+        ball.position = touch.location(in: self)
+        ball.fillColor = color
+        ball.strokeColor = color
+        ball.name = "ball"
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: circleSize + 2)
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.restitution = 0.6
+        ball.physicsBody?.affectedByGravity = true
+        
+        addChild(ball)
     }
     
+    func processUserMotionForUpdate(forUpdate currentTime: CFTimeInterval) {
+        if let data = self.motionManager.accelerometerData {
+            self.physicsWorld.gravity = CGVector(dx: data.acceleration.y * 20, dy: data.acceleration.x * -20)
+        }
+    }
+    
+    func shotBall(ball: SKShapeNode, touch: UITouch) {
+        let dx = touch.location(in: self).x - touch.previousLocation(in: self).x
+        let dy = touch.location(in: self).y - touch.previousLocation(in: self).y
+        
+        ball.physicsBody?.velocity.dx = dx * 70
+        ball.physicsBody?.velocity.dy = dy * 70
+    }
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        // function which gets called before every frame
+        if self.gravity {
+            processUserMotionForUpdate(forUpdate: currentTime)
+        }
     }
 }
